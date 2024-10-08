0c57b6daf136d51ce6a74830f4290492873d4165
achrafwe <106319432+achrafwe@users.noreply.github.com>
2024-10-08 23:03:51 +0200

    latest update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,14 @@
-// next.config.mjs
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-export default {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',  // Ensure static export
+    distDir: 'out',    // Export will be in 'out' folder by default
+    images: {
+      unoptimized: true,  // Since GitHub Pages doesn't support Next.js image optimization
+    },
+    // Ensure the base path points to the GitHub Pages URL
+    basePath: process.env.NODE_ENV === 'production' ? '/Portfo' : '', 
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfo/' : '',
+  };
+  
+  export default nextConfig;
+  
