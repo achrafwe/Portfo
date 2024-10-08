608d12c80a87a7903b3156142004af6eef175296
achrafwe <106319432+achrafwe@users.noreply.github.com>
2024-10-08 22:57:30 +0200

    latest update

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,22 +1,33 @@
 "use client";
 import {AnimatePresence, motion} from "framer-motion";
 import {usePathname} from "next/navigation";
+import { ReactNode } from "react";  // Import ReactNode
 
-const PageTransition = ({children}) => {
+type PageTransitionProps = {
+    children: ReactNode;  // Define the type of children
+};
+
+const PageTransition = ({children}: PageTransitionProps) => {
     const pathname = usePathname();
-    return <AnimatePresence>
-        <div key={pathname}>
-            <motion.div initial={{opacity: 1}} animate={{
-                    opacity: 0, transition: {
-                        delay: 1,
-                        duration: 0.4, ease: "easeInOut"
-                    }
-                }}
-                        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+    return (
+        <AnimatePresence>
+            <div key={pathname}>
+                <motion.div
+                    initial={{ opacity: 1 }}
+                    animate={{
+                        opacity: 0,
+                        transition: {
+                            delay: 1,
+                            duration: 0.4,
+                            ease: "easeInOut",
+                        },
+                    }}
+                    className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
                 />
-            {children}</div>
-                </AnimatePresence>
+                {children}
+            </div>
+        </AnimatePresence>
+    );
+};
 
-
-            };
 export default PageTransition;
