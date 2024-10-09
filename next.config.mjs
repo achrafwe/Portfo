c998cfd534ad9bec283cc1eaec91206ce107c31d
Achraf Elkalchy <106319432+achrafwe@users.noreply.github.com>
2024-10-09 10:07:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',  // Ensure static export
-    distDir: 'out',    // Export will be in 'out' folder by default
-    images: {
-      unoptimized: true,  // Since GitHub Pages doesn't support Next.js image optimization
-    },
-    // Ensure the base path points to the GitHub Pages URL
-    basePath: process.env.NODE_ENV === 'production' ? '/Portfo' : '', 
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfo/' : '',
-  };
+     reactStrictMode:true,
+};
   
   export default nextConfig;
   
